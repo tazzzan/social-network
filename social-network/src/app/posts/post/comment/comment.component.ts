dbee2a5184cf6ea351cb8a59464c5aac05bb6fff
tazzzan <ilja.an@mail.de>
2018-12-08 23:22:52 +0100

    comment component erstellt

---
+++ social-network/src/app/posts/post/comment/comment.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Post} from "../../post.class";
+
+@Component({
+  selector: 'app-comment',
+  templateUrl: './comment.component.html',
+  styleUrls: ['./comment.component.scss']
+})
+export class CommentComponent implements OnInit {
+  @Input() comment: Comment;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
