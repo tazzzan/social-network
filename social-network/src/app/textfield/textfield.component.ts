32fe94b5feccc674015266b015bf4089d89606a0
tazzzan <ilja.an@mail.de>
2018-12-08 20:47:30 +0100

    added textfield posting

---
+++ social-network/src/app/textfield/textfield.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {PostService} from "../posts/postservice.service";
+
+@Component({
+  selector: 'app-textfield',
+  templateUrl: './textfield.component.html',
+  styleUrls: ['./textfield.component.scss']
+})
+
+export class TextfieldComponent implements OnInit {
+
+  constructor(public postService: PostService) { }
+
+  ngOnInit() {
+  }
+
+}
